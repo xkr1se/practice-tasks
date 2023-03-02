bd33293ae215c4aad085371ffae2770eef4bb622
xkr1se <xkr1se@vk.com>
2023-03-02 19:38:50 +0800

    add task-4

--- task-4/src/main/java/ru/xkr1se/Solution.java
+++ task-4/src/main/java/ru/xkr1se/Solution.java
@@ -1,5 +1,16 @@
-package ru.xkr1se;/**
+package ru.xkr1se;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
  * @author xkr1se
  */
 public class Solution {
+    public static String order(String words) {
+        return Arrays.stream(words.split(" "))
+                .sorted(Comparator.comparing(s -> Integer.valueOf(s.replaceAll("\\D", ""))))
+                .reduce((a, b) -> a + " " + b)
+                .get();
+    }
 }
