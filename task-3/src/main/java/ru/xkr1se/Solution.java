b7c7bb958a33a27a95267d48508ed0abf4391537
xkr1se <xkr1se@vk.com>
2023-03-02 19:42:45 +0800

    add solution to task-3

--- task-3/src/main/java/ru/xkr1se/Solution.java
+++ task-3/src/main/java/ru/xkr1se/Solution.java
@@ -1,5 +1,12 @@
-package ru.xkr1se;/**
+package ru.xkr1se;
+
+/**
  * @author xkr1se
  */
 public class Solution {
+
+    public static String makeReadable(int seconds) {
+        return String.format("%02d:%02d:%02d", (seconds / 3600), (seconds / 60 % 60), (seconds % 60));
+    }
+
 }
